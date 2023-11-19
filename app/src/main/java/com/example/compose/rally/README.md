fdab4fbd3fc91a461a8488b3106c804b067b93fb
lloppy <ankudinovapol@gmail.com>
2023-11-19 18:06:58 +0500

    add readme with adb test command

---
+++ app/src/main/java/com/example/compose/rally/README.md
@@ -0,0 +1,4 @@
+Testing screens in terminal:
+
+adb shell am start -d "rally://single_account/Checking" -a android.intent.action.VIEW
+- 
---
+++ app/src/main/java/com/example/compose/rally/widget/MyAppWidgetReceiver.kt
@@ -0,0 +1,8 @@
+package com.example.compose.rally.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class MyAppWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
+}
