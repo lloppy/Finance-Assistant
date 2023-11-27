6f055f61243ee8d07abb5fc49641ab0378a97515
lloppy <ankudinovapol@gmail.com>
2023-11-28 03:29:35 +0500

    add chatGPT without apikey

---
+++ app/src/main/java/com/example/compose/rally/ui/chat/OpenAIApi.kt
@@ -0,0 +1,28 @@
+package com.example.compose.rally.ui.chat
+
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+
+interface OpenAIApi {
+    @Headers("Content-Type: application/json", "Authorization: Bearer sk-")
+    @POST("v1/chat/completions")
+    suspend fun generateResponse(@Body requestBody: OpenAIRequestBody): OpenAIResponse
+}
+
+data class OpenAIRequestBody(
+    val model: String = "gpt-3.5-turbo",
+    val messages: List<Message>,
+    val max_tokens: Int = 50,
+    val n: Int = 1,
+    val temperature: Double = 1.0
+)
+
+data class OpenAIResponse(
+    val choices: List<MessageResponse>
+)
+
+data class MessageResponse(
+    val message: Message
+)
